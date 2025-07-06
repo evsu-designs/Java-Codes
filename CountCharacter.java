07d1a54a0838322785534d8bd4503b8cc929b88e
The-techcodes <myprojectsevsu@gmail.com>
2025-07-06 21:04:48 +0530

    first commit

---
+++ CountCharacter.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class CountCharacter {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in=new Scanner(System.in);
+        String s=in.nextLine();
+        StringBuilder s1=new StringBuilder(s);
+        StringBuffer s2=new StringBuffer(s);
+        System.out.println(s1.reverse());
+         System.out.println(s1.length());
+        
+    }
+}
